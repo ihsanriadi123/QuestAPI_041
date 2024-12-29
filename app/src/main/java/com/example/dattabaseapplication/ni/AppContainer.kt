91c63426019b0f902b006db9843ba457f492736d
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-12-29 18:19:19 +0700

    "Tambahkan kelas MahasiswaContainer untuk inisialisasi Retrofit dan MahasiswaService"

--- app/src/main/java/com/example/dattabaseapplication/ni/AppContainer.kt
+++ app/src/main/java/com/example/dattabaseapplication/ni/AppContainer.kt
@@ -19,6 +19,7 @@ class MahasiswaContainer : AppContainer {
         retrofit.create(MahasiswaService::class.java)
     }
 
+
     override val mahasiswaRepository: MahasiswaRepository by lazy {
         object : MahasiswaRepository {
             override suspend fun getMahasiswa() = mahasiswaService.getAllMahasiswa()
